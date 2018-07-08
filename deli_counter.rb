d5438acbe2efcb7ea5dc1b42edd8582d9928fa1b
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 14:57:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,27 +14,4 @@ def line(katz_deli)
   end
 end
 
-function nowServing(katzDeliLine) {
- if (katzDeliLine.length > 0) {
-   var statement = `Currently serving ${katzDeliLine[0]}.`;
-   katzDeliLine.shift();
-   return statement;
-  }
-  else {
-    return "There is nobody waiting to be served!";
-  }
-}
-
-function currentLine(katzDeliLine) {
-  var array = [];
-  if (katzDeliLine.length > 0) {
-    for (var i = 0; i < katzDeliLine.length; i++) {
-      array[i] = `${i+1}. ${katzDeliLine[i]}`;
-    }
-    return `The line is currently: ${array.join(', ')}`;
-  }
-  else {
-    return "The line is currently empty.";
-  }
-}
 

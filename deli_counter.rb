bc5db261f9c4810292594da8e9b054d8213b124f
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 15:42:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ other_deli = [];
 
 def line(katz_deli)
   if katz_deli.length > 0
-    sentence = "The line is currently: "
+    sentence = "The line is currently:"
     for i in 0..katz_deli.length-1 
-      sentence = sentence + "#{i+1}. #{katz_deli[i]} "
+      sentence = sentence + " #{i+1}. #{katz_deli[i]}"
       end
     puts sentence 
   else 

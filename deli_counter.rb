2d19bfbcbf5367efce91b6f1cd28e4355b410093
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 15:10:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(katz_deli)
   if katz_deli.length > 0
     sentence = "The line is currently: "
     for i in 0..katz_deli.length-1 
-      line = "#{i+1}. #{katz_deli[i]}"
+      line = sentence + "#{i+1}. #{katz_deli[i]}"
       end
-    puts sentence + line 
+    puts line 
   else puts "The line is currently empty."
   end
 end

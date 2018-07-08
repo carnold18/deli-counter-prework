66b04d0f7aa76155e33b41db06f77168cabe289d
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 15:33:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,12 +10,22 @@ def line(katz_deli)
       sentence = sentence + "#{i+1}. #{katz_deli[i]}"
       end
     puts sentence 
-  else puts "The line is currently empty."
+  else 
+    puts "The line is currently empty."
   end
 end
 
 def take_a_number(katz_deli,name)
   katz_deli.push(name)
   puts "Welcome #{name}, you are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
 end
 

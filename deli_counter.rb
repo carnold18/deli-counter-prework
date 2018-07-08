e1fd578677c3a291fcbf4db9e11e8dbf15f025a3
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 14:42:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ other_deli = [];
 def line(katz_deli)
   if katz_deli.length > 0
    katz_deli.each do |i| 
-     katz_deli.push("The line is currently: #{i}. #{katz_deli[i]}.")
+     katz_deli.push("The line is currently: #{katz_deli.index(i)+1}. #{i}.")
    end
   else puts "The line is currently empty."
   end

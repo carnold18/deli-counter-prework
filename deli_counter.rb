37cdad1cd8538487d4d23b00db1edd74d9d44879
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 14:38:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+
+katz_deli = [];
+other_deli = [];
+
+def line(katz_deli)
+  if katz_deli.length > 0
+   katz_deli.each do |i| 
+     katz_deli.push("The line is currently: #{i}. #{katz_deli[i]}."
+   end
+  else puts "The line is currently empty."
+  end
+end
+
+function nowServing(katzDeliLine) {
+ if (katzDeliLine.length > 0) {
+   var statement = `Currently serving ${katzDeliLine[0]}.`;
+   katzDeliLine.shift();
+   return statement;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var array = [];
+  if (katzDeliLine.length > 0) {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      array[i] = `${i+1}. ${katzDeliLine[i]}`;
+    }
+    return `The line is currently: ${array.join(', ')}`;
+  }
+  else {
+    return "The line is currently empty.";
+  }
+}
+
